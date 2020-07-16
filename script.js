19b09471be3294dca2ba80e5b96986bcdf54157d
Caroline Strickland <carolinelstrickland@outlook.com>
2020-07-16 12:09:50 -0400

    working through javascript

---
+++ script.js
@@ -0,0 +1,11 @@
+function generate(){
+
+    // set password length
+    let length = document.getElementById("slider").value;
+    
+    // values allowed
+    let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()-_=+~`[{]};:,<.>/?";
+
+    let password = "";
+
+}
